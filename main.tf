4186f43f051c8a04261a3102607ac396d6d79caa
Manoj Kumar Duraipandi <157309229+manoj-andino@users.noreply.github.com>
2024-02-06 15:16:09 +0530

    Add variables for region and global prefix

--- main.tf
+++ main.tf
@@ -10,51 +10,51 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
-resource "aws_vpc" "msk_inventory_spike_vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "MSK Inventory Spike VPC"
+    Name = "${var.global_prefix} VPC"
   }
 }
 
 resource "aws_subnet" "public_subnets" {
-  count = length(var.vpc_subnets)
-  vpc_id = aws_vpc.msk_inventory_spike_vpc.id
-  cidr_block = element(var.vpc_subnets, count.index)
+  count             = length(var.vpc_subnets)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = element(var.vpc_subnets, count.index)
   availability_zone = element(var.azs, count.index)
 
   tags = {
-    Name = "Public subnet ${count.index+1}"
+    Name = "${var.global_prefix} public subnet ${count.index + 1}"
   }
 }
 
-resource "aws_internet_gateway" "msk_inventory_spike_igw" {
-  vpc_id = aws_vpc.msk_inventory_spike_vpc.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "MSK Inventory Spike VPC IGW"
+    Name = "${var.global_prefix} IGW"
   }
 }
 
-resource "aws_route_table" "misv_second_route_table" {
-  vpc_id = aws_vpc.msk_inventory_spike_vpc.id
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.msk_inventory_spike_igw.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 
   tags = {
-    Name = "MIS Second route table"
+    Name = "${var.global_prefix} second RT"
   }
 }
 
-resource "aws_route_table_association" "misv_second_route_table_association" {
-  count = length(var.vpc_subnets)
-  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
-  route_table_id = aws_route_table.misv_second_route_table.id
+resource "aws_route_table_association" "my_route_table_association" {
+  count          = length(var.vpc_subnets)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.my_route_table.id
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
+variable "region" {
+  description = "Region to use for AWS resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "global_prefix" {
+  description = "Prefix to use for AWS resources"
+  type        = string
+  default     = "msk_inventory_spike"
+}
+
 variable "vpc_subnets" {
-  type = list(string)
+  type        = list(string)
   description = "Subnet CIDR values"
-  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "azs" {
- type        = list(string)
- description = "Availability Zones"
- default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
